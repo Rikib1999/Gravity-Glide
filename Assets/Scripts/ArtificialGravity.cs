426e586b691f2f32eb56d8a808d66076ab867304
Rikib1999 <74504682+Rikib1999@users.noreply.github.com>
2023-11-25 17:46:24 +0100

    update

--- Assets/Scripts/ArtificialGravity.cs
+++ Assets/Scripts/ArtificialGravity.cs
@@ -3,23 +3,50 @@ using UnityEngine;
 
 public class ArtificialGravity : MonoBehaviour
 {
+    [SerializeField]
+    private float radius = 5.0f;
+    public float Radius
+    {
+        get => radius;
+        set
+        {
+            radius = value;
+            SetRadius(value);
+        }
+    }
+
     public float strength = 0.0f;
-    public float radius = 5.0f;
     public bool attractiveForce = true;
+
+    private ParticleSystem pm;
+
+    private void Awake()
+    {
+        GetComponent<CircleCollider2D>().radius = Radius;
+        pm = GetComponentInChildren<ParticleSystem>();
+    }
 
     private void Start()
     {
-        GetComponent<CircleCollider2D>().radius = radius;
-        var pm = GetComponentInChildren<ParticleSystem>();
-        
+        SetParticles();
+    }
+
+    private void SetParticles()
+    {
         var m = pm.main;
-        m.startLifetime = attractiveForce ? radius * 0.3f : radius / 3f;
+        m.startLifetime = attractiveForce ? Radius * 0.3f : Radius / 3f;
 
         if (attractiveForce)
         {
             var s = pm.shape;
-            s.radius = radius * 0.65f;
+            s.radius = Radius * 0.65f;
         }
+    }
+
+    private void SetRadius(float value)
+    {
+        Radius = Mathf.Clamp(value, 2f, 8f);
+        SetParticles();
     }
 
     void OnTriggerStay2D(Collider2D other)
@@ -32,7 +59,7 @@ public class ArtificialGravity : MonoBehaviour
 
             float distance = Vector2.Distance(transform.position, other.transform.position);
 
-            float force = (radius - distance) / radius;
+            float force = (Radius - distance) / Radius;
 
             other.GetComponent<Rigidbody2D>().AddForce(direction.normalized * strength * force * force, ForceMode2D.Force);
         }
@@ -41,6 +68,6 @@ public class ArtificialGravity : MonoBehaviour
     private void OnDrawGizmos()
     {
         Handles.color = attractiveForce ? Color.red : Color.green;
-        Handles.DrawWireDisc(transform.position + transform.forward, transform.forward, radius);
+        Handles.DrawWireDisc(transform.position + transform.forward, transform.forward, Radius);
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,5 +2,29 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject attractivePrefab;
+    public GameObject repulsivePrefab;
 
+    private ArtificialGravity lastArtificialGravity;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+
+            lastArtificialGravity = Instantiate(attractivePrefab, pos, Quaternion.identity).GetComponent<ArtificialGravity>();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+
+            lastArtificialGravity = Instantiate(repulsivePrefab, pos, Quaternion.identity).GetComponent<ArtificialGravity>();
+        }
+
+        if (lastArtificialGravity != null) lastArtificialGravity.Radius += Input.mouseScrollDelta.y;
+    }
 }
