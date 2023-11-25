ebb02a271315647352a37e82c060d98137e618f8
Rikib1999 <74504682+Rikib1999@users.noreply.github.com>
2023-11-25 18:21:31 +0100

    PlayStop button

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject playerPrefab;
     public GameObject attractivePrefab;
     public GameObject repulsivePrefab;
 
+    public Transform spawnPoint;
+
+    private GameObject player;
     private ArtificialGravity lastArtificialGravity;
+
+    public bool IsPlaying => player != null;
 
     private void Update()
     {
+        if (IsPlaying) return;
+
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -28,5 +36,19 @@ public class LevelManager : MonoBehaviour
         }
 
         if (lastArtificialGravity != null && Input.mouseScrollDelta.y != 0) lastArtificialGravity.Radius += Input.mouseScrollDelta.y;
+    }
+
+    public void PlayStop()
+    {
+        if (IsPlaying)
+        {
+            Destroy(player);
+            player = null;
+        }
+        else
+        {
+            player = Instantiate(playerPrefab, spawnPoint.position, Quaternion.identity);
+            if (lastArtificialGravity != null) lastArtificialGravity.radiusGraphics.GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
 }
